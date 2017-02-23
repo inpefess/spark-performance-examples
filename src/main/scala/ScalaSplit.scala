3de0544cd49c399288eeedbb8ab40338082bb1f1
Boris Shminke <schminkeba@gmail.com>
2017-02-23 13:19:29 +0300

    added scalastyle plugin and scaladoc comments

--- src/main/scala/ScalaSplit.scala
+++ src/main/scala/ScalaSplit.scala
@@ -1,9 +1,15 @@
 import org.apache.spark.sql.api.java.UDF1
 
-/*
-  This is an extremely simple UDF that work similarly to org.apache.spark.sql.functions.split
- */
+/**
+  * This is an extremely simple UDF that work similarly to org.apache.spark.sql.functions.split
+  */
 class ScalaSplit extends UDF1[String, Array[String]] {
+  /**
+    * a wrapper of a built-in split method
+    * @param str a string to split by a whitespace symbol
+    * @throws it just must throw some exception to be register in Spark
+    * @return an array of strings
+    */
   @throws[Exception]
   def call(str: String): Array[String] = str.split(" ")
 }
